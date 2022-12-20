7634f535ef2989e0c415ff45d12a303e61500433
William <kwasidev@protonmail.com>
2022-12-20 15:19:22 +0200

    Fix token authentication issue :bug:
    Signed-off-by: William <kwasidev@protonmail.com>

--- middlewares/index.js
+++ middlewares/index.js
@@ -2,11 +2,12 @@ const jwt = require('jsonwebtoken');
 const checkAuth = (req, res, next) => {
     const accessToken = req.headers['token'];
     jwt.verify(accessToken, process.env.JWT_SECRET, (err, decoded) => {
-        req.userId = decoded.userId;
+      
         if (err) {
             res.status(403).json({ message: 'Invalid Token' });
         
         } else {
+            req.userId = decoded.userId;
             next();
         }
     });
