58ad329af5ad184470b683ba58fe1936556a26aa
William <kwasidev@protonmail.com>
2022-12-20 15:29:04 +0200

    Check if username and password is present
    Signed-off-by: William <kwasidev@protonmail.com>

--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,9 @@ router.get('/', (req, res, next) => {
 });
 
 router.post('/login', async (req, res, next) => {
+  if (!req.body.username || !req.body.password) {
+    res.status(400).json({success: false, error: "Username and Password is required"});
+  }
   let results = await UserService.login(req.body.username, req.body.password);
   if (results.success) {
     res.status(200).json(results);
@@ -26,8 +29,4 @@ router.post('/refreshToken', async (req, res, next) => {
     res.status(401).json(results);
   }
 });
-
-
-
-
 module.exports = router;
